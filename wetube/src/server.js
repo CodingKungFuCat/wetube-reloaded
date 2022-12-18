fab90a019f1ea7efc2187a71053daeddffd30654
coding cat <wow0372@naver.com>
2022-12-18 20:28:08 +0900

    new commit

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -3,5 +3,7 @@ import nodemon from "nodemon";
 
 const app = express();
 const PORT = 4000;
+const handleHome = (req, res) => console.log("somebody is going to go home");
+app.get("/", () => handleHome);
 
 app.listen(PORT, () => console.log(`Server Listening on port http://localhost:${PORT}`))
