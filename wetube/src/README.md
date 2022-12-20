bd058cfd333729002ceb0ac18bbacb7a3977a8a8
coding cat <wow0372@naver.com>
2022-12-20 15:46:49 +0900

    today commit

---
+++ wetube/src/README.md
@@ -0,0 +1,19 @@
+# wetube Url Design
+
+/ -> Home
+/join -> Join
+/login -> Login
+<!-- -----------------------global router------------------------------------------ -->
+
+/users/edit -> Edit Useres
+/users/delete -> Delete Users
+
+<!-- -----------------------usersl router------------------------------------------ -->
+
+
+/videos/watch -> Watch video
+/videos/edit -> Edit videos
+/videos/delete -> Delete videos
+/videos/comments -> Comments on a video
+/videos/comments/delete -> Delete comments on a video
+<!-- -----------------------videos router------------------------------------------ -->
--- wetube/src/server.js
+++ wetube/src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import nodemon from "nodemon";
+import morgan from "morgan"
 
 const app = express();
+const loggerMiddleWare = morgan("dev");
 const PORT = 4000;
+
+
+
 const handleLogin = (req, res) => {
     return res.send("Login Here")
 }
@@ -18,32 +23,11 @@ const handleHome = (req, res) => {
 
 };
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`)
-    next();
-    // next는 30번째 코드에 있는 gossipMiddleWare 다음에 나오는 handleHome 즉 다음 함수가 작동되게 해준다 => 동기화방식
-    //그렇기 때문에 next를 빼주면 gossipMiddleware만 작동된다.
-};
 
-const privateMiddleWare = (req, res) => {
-    return res.send("여기는 비밀의 방이야")
-};
 
-const protector = (req, res, next) => {
-    const url = req.url
-    if (url === "/protected") {
-        console.log("허용되지 않는 페이지 입니다.")
-        res.send("허용되지 않은 페이지 입니다.")
-    } else {
-        console.log("허용되는 페이지 입니다.")
-        next();
-    }
-}
 
 // protected  된 페이지를 들어가려고 하면 if 문에 의해 들어갈 수 없고 아닌 경우 허용됨이 표시되게 하기
-app.use(logger)
-app.use(protector)
+app.use(loggerMiddleWare)
 app.get("/", handleHome);
 app.get("/login", handleLogin)
-app.get("/protected", privateMiddleWare)
 app.listen(PORT, () => console.log(`Server Listening on port http://localhost:${PORT}`))
