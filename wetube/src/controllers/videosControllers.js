cd5285d96c6f546edb005e37515fb42865436c87
coding cat <wow0372@naver.com>
2022-12-24 21:29:31 +0900

    221224 TIL

--- wetube/src/controllers/videosControllers.js
+++ wetube/src/controllers/videosControllers.js
@@ -1,9 +1,9 @@
-const fakeUsers = {
-    username: "jinwoo",
-    loggedIn: true
-}
 
-export const trending = (req, res) => res.render("home", { pageTitle: "home", fakeUsers })
+
+export const trending = (req, res) => {
+    const videos = []
+    res.render("home", { pageTitle: "home", videos })
+}
 export const see = (req, res) => res.render("watch", { pageTitle: "watch" })
 export const edit = (req, res) => res.render("edit", { pageTitle: "edit" })
 export const Search = (req, res) => res.send("Search Videos", { pageTitle: "search" })
