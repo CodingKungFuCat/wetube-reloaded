65b97e16918e15feb944ac7a394d17353ba05010
coding cat <wow0372@naver.com>
2022-12-24 16:58:45 +0900

    something is removed

--- wetube/src/controllers/videosControllers.js
+++ wetube/src/controllers/videosControllers.js
@@ -1,6 +1,6 @@
 export const trending = (req, res) => res.render("home")
-export const see = (req, res) => res.send(`watch video ${req.params.id}`)
-export const edit = (req, res) => res.send("edit video")
+export const see = (req, res) => res.render("watch")
+export const edit = (req, res) => res.render("edit")
 export const Search = (req, res) => res.send("Search Videos")
 export const Upload = (req, res) => res.send("Upload Video")
 export const Delete = (req, res) => res.send("Delete Video")
